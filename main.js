71d170549697d7b6274ec19c5ced30b1f178003c
Asm <asm.khan.sarib98@gmail.com>
2023-04-09 14:25:16 +0600

    setup transaction page and main js file

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const text1=document.getElementById('text1')
-const chart1=document.getElementById('pie_chart')
-const chart2=document.getElementById('bar_chart')
-const chart3=document.getElementById('remove_chart')
+const text2=document.getElementById('text2')
+const button1=document.getElementById('pie_chart')
+const button2=document.getElementById('bar_chart')
+const button3=document.getElementById('remove_chart')
+const button4=document.getElementById('all_info')
 
 document.addEventListener('DOMContentLoaded', function(){
-    chart1.addEventListener('click', function(){
-        text1.innerHTML="Pie chart"
-    })
-    chart2.addEventListener('click', function(){
-        text1.innerHTML="Bar chart"
-    })
-    chart3.addEventListener('click', function(){
-        text1.innerHTML="Remove chart"
-    })
+    if(button1!=null){
+        button1.addEventListener('click', function(){
+            text1.innerHTML="Pie chart"
+        })
+    }
+    if(button2!=null){
+        button2.addEventListener('click', function(){
+            text1.innerHTML="Bar chart"
+        })
+    }
+    if(button3!=null){
+        button3.addEventListener('click', function(){
+            text1.innerHTML="Remove chart"
+        })
+    }
+    if(button4!=null){
+        button4.addEventListener('click', function(){
+            text2.innerHTML="Show All Transactions"
+        })
+    }
 })
