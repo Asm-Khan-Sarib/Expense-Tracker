786a61b0eb0aeecf1d427a87534be68235ba9ed2
Asm <asm.khan.sarib98@gmail.com>
2023-04-15 15:05:25 +0600

    added delet buttons work

--- jsForDeletTransection.js
+++ jsForDeletTransection.js
@@ -5,21 +5,47 @@ const submit_button = document.getElementById('submit')
 const text = document.getElementById('text')
 const text2 = document.getElementById('text2')
 const form = document.querySelector('.give_data')
+let local_data=[]
+
+function quick_delet(x){
+    local_data = JSON.parse(localStorage.getItem('user_info'))
+    if (local_data != null) {
+        localStorage.removeItem('user_info');
+        if(x===1){
+            local_data=local_data.slice(0,-1)
+            text2.innerHTML = "1 delet"
+        }
+        else if(x===5){
+            local_data=local_data.slice(0,-5)
+            text2.innerHTML = "5 delet"
+        }
+        if(x!='all'  && (local_data.length>=1)){
+            localStorage.setItem('user_info', JSON.stringify(local_data))
+        }
+        else{
+            text2.innerHTML = "All delet"
+        }
+    }
+    else {
+        text2.innerHTML = "You have no Trunsection left to delet"
+    }
+}
 
 document.addEventListener('DOMContentLoaded', function (){
     submit_button.addEventListener('click', function () {
-        text.innerHTML = "Please write only one transection id at a time"
+        text.innerHTML = "Transection not found <br>Please write only one transection id at a time"
     })
     form.addEventListener('submit', (event) => {
         event.preventDefault()
+        let delet_id = document.querySelector('#delet_id').value
     })
     delet.addEventListener('click', function () {
-        text2.innerHTML = "delet all transection"
+        quick_delet('all')
     })
     delet1.addEventListener('click', function () {
-        text2.innerHTML = "delet 1 transection"
+        quick_delet(1)
     })
     delet5.addEventListener('click', function () {
-        text2.innerHTML = "delet 5 transection"
+        quick_delet(5)
     })
 })
